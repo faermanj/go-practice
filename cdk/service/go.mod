3645b6b10b49e3063274bd02cb74fc04f949109b
Julio Faerman <356476+faermanj@users.noreply.github.com>
2025-03-14 14:55:11 -0300

    wip

--- cdk/service/go.mod
+++ cdk/service/go.mod
@@ -3,7 +3,7 @@ module service
 go 1.18
 
 require (
-  github.com/aws/aws-cdk-go/awscdk/v2 v2.181.1
-  github.com/aws/constructs-go/constructs/v10 v10.0.5
-  github.com/aws/jsii-runtime-go v1.29.0
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.181.1
+	github.com/aws/constructs-go/constructs/v10 v10.4.2
+	github.com/aws/jsii-runtime-go v1.106.0
 )
--- scripts/app-image.sh
+++ scripts/app-image.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRC_DIR="$(dirname "$DIR")"
+
+version_x=$(cat version.x.txt)
+version_y=$(cat version.y.txt)
+version_z=$(date +%Y%m%d%H%M)
+version="$version_x.$version_y.$version_z"
+
+REGISTRY_USERNAME=${REGISTRY_USERNAME:-$USER}
+IMAGE_TAG=$REGISTRY_USERNAME/go-practice:$version
+
+cd $SRC_DIR
+
+echo "Building $IMAGE_TAG"
 docker build \
     --debug \
     --progress=plain \
     --no-cache \
     -f Containerfile  \
-    -t go-practice \
+    -t $IMAGE_TAG \
     .
+
+echo "Pushing $IMAGE_TAG"
+docker push "$IMAGE_TAG"
+
+echo "Image[$IMAGE_TAG] built and pushed"
